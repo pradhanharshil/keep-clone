603c1d489c06d65af382b6434aed0e9cfcb74d22
harshilpradhan-simformsolutions <harshilpradhan@simformsolutions.com>
2021-03-05 11:23:19 +0530

    Improve styling of input

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,10 +4,10 @@ function Form({ onSubmit }) {
     return (
         <form className="flex flex-col items-center ml-auto mr-auto mt-5 mb-5 w-96" onSubmit={onSubmit}>
             <input
-                className="p-2 border w-full rounded font-bold text-black"
+                className="p-2 border w-full rounded font-bold text-black focus:border-yellow-500"
                 placeholder="Take a note..." />
             <textarea 
-                className="p-2 border w-full"
+                className="p-2 border w-full focus:border-yellow-500"
                 placeholder="info"/>
             <button 
                 className="bg-yellow-400 text-white rounded-full mt-1 mb-1 text-2xl bold h-8 w-8 text-center"
