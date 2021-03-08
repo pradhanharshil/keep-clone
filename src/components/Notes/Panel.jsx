a663b9bf02da8b84cc8878bf9ff44c78c88d0e68
harshilpradhan-simformsolutions <harshilpradhan@simformsolutions.com>
2021-03-08 16:49:28 +0530

    Improve icons

--- src/components/Notes/Panel.jsx
+++ src/components/Notes/Panel.jsx
@@ -3,13 +3,15 @@ import ColorIcon from "./ColorIcon";
 import Icon from "./Icon";
 import pin from "../../assets/icons/pin.png";
 import unpin from "../../assets/icons/unpin.png";
-import trash from "../../assets/icons/trash.webp";
+import trash from "../../assets/icons/trash.png";
+import edit from "../../assets/icons/edit.png";
 import { observer } from "mobx-react-lite";
 
-function Panel({ setColor, pinNote, deleteNote, isPinned }) {
+function Panel({ setColor, pinNote, deleteNote, isPinned, editNote }) {
     return (
         <div className="flex flex-row m-1">
             <Icon src={isPinned ? unpin : pin} clickHandler={pinNote} height="5" width="5" />
+            <Icon src={edit} clickHandler={editNote} height="5" width="5" />
             <Icon src={trash} clickHandler={deleteNote} height="5" width="5" />
             {Object.values(colors).map(key => <ColorIcon key={key} height="5" width="5" color={key} handler={setColor} />)}
         </div>
